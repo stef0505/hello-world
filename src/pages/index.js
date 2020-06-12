bea2d5ace37d5ae3637d71e7f8ced441e9500172
Stephane Peyras <peyras.ste@hotmail.fr>
2020-06-12 03:51:34 +0200

    test boostrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+ import { ListGroup } from 'react-bootstrap';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return 
+  <div>
+  <ul class="list-group">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+</div>
 }
